714e1e31c1ea1d48f18789bf508f38e77a2537c4
hachimabdel <abdelhachim@icloud.com>
2023-05-12 07:10:30 +0200

    kjb

--- HTML/WorkSpace Spring/ProyectoFinal/src/main/java/com/salesianostriana/dam/ProyectoFinal/controller/CategoriaController.java
+++ HTML/WorkSpace Spring/ProyectoFinal/src/main/java/com/salesianostriana/dam/ProyectoFinal/controller/CategoriaController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.salesianostriana.dam.ProyectoFinal.model.Categoria;
@@ -34,6 +37,55 @@ public class CategoriaController {
 	public String nuevaCategoria(Model model) {
 		model.addAttribute("categoria", new Categoria());
 		return "admin/formulario-categoria";
+	}
+	
+	@PostMapping("/nueva/submit")
+	public String submitNuevaCategoria(@ModelAttribute("categoria") Categoria categoria, Model model) {
+		
+		categoriaService.save(categoria);
+		
+		//En el redirect hay que poner la ruta completa del controller al que queremos ir, 
+		//incluyendo lo escrito dentro del @RequestMapping del comienzo de la clase
+		return "redirect:/admin/categoria/";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String editarCategoria(@PathVariable("id") Long id, Model model) {
+		
+		Categoria categoria = categoriaService.findById(id);
+		
+		
+		/*Si te fijas en el método nuevaCategoría, al model le pasamos una new categoria ( ) vacía porque es el de agregar nueva categoría.
+		 * Ahora pasamos la categoría que nos devuelve el método findById (id) que está justo encima de este comentario, por lo que, el 
+		 * formulario ya se pinta con los datos de esta categoría buscada*/
+		if (categoria != null) {
+			model.addAttribute("categoria", categoria);
+			return "admin/form-categoria";
+		} else {
+			return "redirect:/admin/categoria/";
+		}
+		
+	}
+	
+	@GetMapping("/borrar/{id}")
+	public String borrarCategoria(@PathVariable("id") Long id, Model model) {
+		
+		Categoria categoria = categoriaService.findById(id);
+		
+		if (categoria != null) {
+			
+			if (productoService.numeroProductosCategoria(categoria) == 0) {
+				categoriaService.delete(categoria);				
+			} else {
+				
+			//Se ha agregado el parámetro error con valor true a la ruta	
+				return "redirect:/admin/categoria/?error=true";
+			}
+			
+		} 
+
+		return "redirect:/admin/categoria/";
+			
 	}
 
 }
--- HTML/WorkSpace Spring/ProyectoFinal/src/main/java/com/salesianostriana/dam/ProyectoFinal/controller/ProductoController.java
+++ HTML/WorkSpace Spring/ProyectoFinal/src/main/java/com/salesianostriana/dam/ProyectoFinal/controller/ProductoController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.salesianostriana.dam.ProyectoFinal.model.Producto;
 import com.salesianostriana.dam.ProyectoFinal.service.CategoriaService;
 import com.salesianostriana.dam.ProyectoFinal.service.ProductoService;
 
@@ -22,7 +25,51 @@ public class ProductoController {
 	@GetMapping("/")
 	public String index(Model model) {
 		model.addAttribute("productos", productoService.findAll());
-		return "admin/list-producto";
+		return "admin/listado-producto";
+	}
+
+	@GetMapping("/nuevo")
+	public String nuevaProducto(Model model) {
+
+		model.addAttribute("producto", new Producto());
+		model.addAttribute("categorias", categoriaService.findAll());
+		return "admin/form-producto";
+	}
+
+	@PostMapping("/nuevo/submit")
+	public String submitNuevoProducto(Producto producto, Model model) {
+
+		productoService.save(producto);
+		return "redirect:/admin/producto/";
+
+	}
+
+	@GetMapping("/editar/{id}")
+	public String editarProducto(@PathVariable("id") Long id, Model model) {
+
+		Producto producto = productoService.findById(id);
+
+		if (producto != null) {
+			model.addAttribute("producto", producto);
+			model.addAttribute("categorias", categoriaService.findAll());
+			return "admin/form-producto";
+		} else {
+			return "redirect:/admin/producto/";
+		}
+
+	}
+
+	@GetMapping("/borrar/{id}")
+	public String borrarProducto(@PathVariable("id") Long id, Model model) {
+
+		Producto producto = productoService.findById(id);
+
+		if (producto != null) {
+			productoService.delete(producto);
+		}
+
+		return "redirect:/admin/producto/";
+
 	}
 
 }
